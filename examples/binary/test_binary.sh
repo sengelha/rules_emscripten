736d2092b093ccf183c72642923eb5ba3742a0f9
Steven Engelhardt <sengelha@gmail.com>
2021-07-22 22:36:24 -0500

    Save work

--- examples/binary/test_binary.sh
+++ examples/binary/test_binary.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+EMCC_BINARY=$1
+
 EXPECTED_OUTPUT="Hello world!"
-ACTUAL_OUTPUT=$(node examples/binary/binary.js)
+ACTUAL_OUTPUT=$($EMCC_BINARY)
 
 if [[ "$ACTUAL_OUTPUT" != "$EXPECTED_OUTPUT" ]]; then
   echo "ERROR: Expected $EXPECTED_OUTPUT, got $ACTUAL_OUTPUT" 1>&2
