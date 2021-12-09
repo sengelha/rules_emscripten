4a863bc07d755bec8c3f5f718fb6eac5a9118586
Steven Engelhardt <sengelha@gmail.com>
2021-12-08 18:40:33 -0600

    chg: Rename examples/c_module to examples/module

--- examples/module/test_module.sh
+++ examples/module/test_module.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 EXPECTED_OUTPUT="33"
-ACTUAL_OUTPUT=$(cd examples/c_module && node --preserve-symlinks-main test_c_module.js)
+ACTUAL_OUTPUT=$(cd examples/module && node --preserve-symlinks-main test_module.js)
 
 if [[ "$ACTUAL_OUTPUT" != "$EXPECTED_OUTPUT" ]]; then
   echo "ERROR: Expected $EXPECTED_OUTPUT, got $ACTUAL_OUTPUT" 1>&2
